2a8565290649952c61fa16bf4ab7f130a4f21652
Lisias T <me@lisias.net>
2018-10-17 02:01:13 -0300

    updating automatin scripts

--- publish.sh
+++ publish.sh
@@ -4,4 +4,6 @@ source ./CONFIG.inc
 
 VERSIONFILE=$PACKAGE.version
 
-scp -i $SSH_ID ./GameData/net.lisias.ksp/$VERSIONFILE $SITE:/$TARGETPATH
+scp -i $SSH_ID ./GameData/$PACKAGE/$VERSIONFILE $SITE:/$TARGET_CONTENT_PATH
+scp -i $SSH_ID ./GameData/$PACKAGE/README.md $SITE:/$TARGET_CMS_PATH/$PACKAGE.md
+scp -i $SSH_ID -rp "./PR_material/${PACKAGE}" $SITE:/${TARGET_CONTENT_PATH}PR_material/
