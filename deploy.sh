b97979667a86e79dfd0ae5f172a8d7168194dfd7
Lisias T <me@lisias.net>
2018-10-19 06:53:45 -0300

    Revert "Avoiding distributing DLL for TweakableEverything until further notice from its maintainers"
    This reverts commit 7dc6980186598ce1e7b03068965255773e91fb7b.

--- deploy.sh
+++ deploy.sh
@@ -56,6 +56,5 @@ done
 deploy_dev Scale_Redist
 
 # HACK! See how to do it properly later.
-#TARGETBINDIR="TweakableEverything/Plugins_TweakScale" check
-#TARGETBINDIR="TweakableEverything/Plugins_TweakScale" deploy Scale_TweakableEverything
-# Note: Avoiding redistributing the TweakableEverything until further notice from its Maintainers
+TARGETBINDIR="TweakableEverything/Plugins_TweakScale" check
+TARGETBINDIR="TweakableEverything/Plugins_TweakScale" deploy Scale_TweakableEverything
