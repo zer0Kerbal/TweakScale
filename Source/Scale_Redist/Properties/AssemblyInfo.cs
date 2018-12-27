7debadf3c739c89f48211eb5a6b0656aab157322
Lisias T <me@lisias.net>
2018-12-27 19:37:48 -0200

    Refactring: Adding IFactory20, IRescalable20 and IDryCost20 to the Scale_Redist project

--- Source/Scale_Redist/Properties/AssemblyInfo.cs
+++ Source/Scale_Redist/Properties/AssemblyInfo.cs
@@ -38,5 +38,14 @@ using System.Runtime.InteropServices;
 // It does not depend on KSP and has not been touched since biotronic wrote it.
 // So its version should stay at 1.0, to make clear that other mods do not need to compile against a new version
 // (pellinor)
-[assembly: AssemblyVersion("1.0")]
+//
+// On the other hand, TweakScale is on a corner. It's not feasible to update third-party add-ons to fix problems
+// related to "unholly interactions between add-ons", but it's not convenient to keep things inside TweakScale and
+// take the heat everytime someone else borks somehow.
+// I see no other way out of this mess but to deprecate the original Scale_Redist and promote a new one, not to be used
+// by the add-ons themselves, but by specialized 'integrators" that should be developed apart from TweakScale.
+// If some add-one borks, instead of "fixing" TweakScale (and taking the blame), the fix should go to the proper "integrator".
+// TweakScale will have a lot less updates this way, and so we don't risk breaking up what's working fine for everybody else.
+// (lisias)
+[assembly: AssemblyVersion("1.1")]
 [assembly: AssemblyFileVersion(TweakScale.Version.Number)]
--- Source/Scale_Redist/Updater.cs
+++ Source/Scale_Redist/Updater.cs
@@ -1,10 +1,36 @@
-﻿namespace TweakScale
+﻿using System.Collections.Generic;
+using KSP;
+
+namespace TweakScale
 {
     public interface IRescalable
     {
         void OnRescale(ScalingFactor factor);
     }
-    public interface IRescalable<T> : IRescalable
+    public interface IRescalable<T> : IRescalable{}
+
+    public interface IFactory20
+	{
+        int getPriority();
+        bool isSupported(Part part);
+        HashSet<string> getSupportedModules();
+        HashSet<string> getUnsupportedModules();
+        
+        IRescalable20 createRescalableFor(Part part);
+        IDryCost20 createDryCostFor(Part part);
+	}
+    public interface IFactory20<T> : IFactory20{}
+
+    public interface IRescalable20 : IRescalable
     {
+        void OnUpdate();
+        void OnShipModified();
     }
+    public interface IRescalable20<T> : IRescalable20 {}
+
+    public interface IDryCost20
+	{
+        float calculate();
+	}
+    public interface IDryCost20<T> : IDryCost20 {}
 }
