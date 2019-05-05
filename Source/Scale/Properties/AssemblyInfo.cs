010cfdccfc16ed6f38e10d8045859ea121255cdf
Lisias T <me@lisias.net>
2019-05-05 15:00:47 -0300

    Forgotten Legal Mambo Jambo

--- Source/Scale/Properties/AssemblyInfo.cs
+++ Source/Scale/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Scale")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("L Aerospace/KSP Division")]
 [assembly: AssemblyProduct("Scale")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2018-2019 LisiasT")]
+[assembly: AssemblyTrademark("TweakScale™ by Gaius Goodspeed, Biotronic, Pellinor, LisiasT")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
